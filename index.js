301a57c117a2f1f648c27e5af71e7456b21f5106
sabbirsiddique <sabbirsiddique6798@gmail.com>
2023-11-08 19:07:06 +0600

    delete funtionality added

--- index.js
+++ index.js
@@ -51,6 +51,14 @@ const client = new MongoClient(uri, {
       const result = await foodCollection.insertOne(foodAdded);
       res.send(result);
   })
+
+
+  app.delete('/fooditems/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await foodCollection.deleteOne(query);
+    res.send(result);
+})
       // const orderCollection = client.db('eateryevo').collection('orders')
 
       // const gateman = (req,res)=>{
